e416578b506e4bf06e1a887d4034127e774266a4
Oleg Kryuchikhin <adenos90@gmail.com>
2025-04-15 18:01:53 +0300

    End 9 sprint

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render, get_object_or_404, redirect
+# from django.shortcuts import render, get_object_or_404, redirect
+# from django.core.paginator import Paginator
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView, DetailView)
+from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
 
 
+class BirthdayMixin:
+    model = Birthday
+
+
+'''
 def birthday(request, pk=None):
-    '''# if request.GET:
+    # if request.GET:
     #     form = BirthdayForm(request.GET)
     #     if form.is_valid():
     #         pass
     # else:
-    #     form = BirthdayForm()'''
+    #     form = BirthdayForm()
     # Если в запросе указан pk (если получен запрос на редактирование объекта):
     if pk is not None:
         # Получаем объект модели или выбрасываем 404 ошибку.
@@ -27,7 +36,7 @@ def birthday(request, pk=None):
         # Файлы, переданные в запросе, указываются отдельно.
         files=request.FILES or None,
         instance=instance,
-        )
+    )
     context = {'form': form}
     if form.is_valid():
         form.save()
@@ -37,14 +46,50 @@ def birthday(request, pk=None):
         context.update({'birthday_countdown': birthday_countdown})
 
     return render(request, 'birthday/birthday.html', context)
+'''
 
 
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    # model = Birthday
+    # # Этот класс сам может создать форму на основе модели!
+    # # Нет необходимости отдельно создавать форму через ModelForm.
+    # # Указываем поля, которые должны быть в форме:
+    # # fields = '__all__'
+    # # Указываем имя формы если она созана с виджетами и клинами:
+    # form_class = BirthdayForm
+    # template_name = 'birthday/birthday.html'
+    # # Указываем namespace:name страницы,куда будет перенаправлен пользователь
+    # # после создания объекта:
+    # success_url = reverse_lazy('birthday:list')
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    # model = Birthday
+    # form_class = BirthdayForm
+    # template_name = 'birthday/birthday.html'
+    # success_url = reverse_lazy('birthday:list')
+    form_class = BirthdayForm
+
+
+'''
 def birthday_list(request):
-    birthdays = Birthday.objects.all().order_by('id')
-    context = {'birthdays': birthdays}
+    birthdays = Birthday.objects.order_by('id')
+    paginator = Paginator(birthdays, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
+'''
 
 
+class BirthdayListView(ListView, BirthdayMixin):
+    # model = Birthday
+    ordering = 'id'
+    paginate_by = 3
+
+
+'''
 def delete_birthday(request, pk):
     instance = get_object_or_404(Birthday, pk=pk)
     # В форму передаём только объект модели;
@@ -58,3 +103,24 @@ def delete_birthday(request, pk):
         # ...и переадресовываем пользователя на страницу со списком записей.
         return redirect('birthday:list')
     return render(request, 'birthday/birthday.html', context)
+'''
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    # model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        return context
+
--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from birthday.models import Birthday
 
 
-def homepage(request):
-    return render(request, 'pages/index.html')
+class Homepage(TemplateView):
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь ключ total_count;
+        # значение ключа — число объектов модели Birthday.
+        context["total_count"] = Birthday.objects.count()
+        return context
+    
+
+
+# def homepage(request):
+#     return render(request, 'pages/index.html')
