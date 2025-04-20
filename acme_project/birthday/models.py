0861e13ccd56445a607bc5acdea3b9a9765b0577
Oleg Kryuchikhin <adenos90@gmail.com>
2025-04-20 17:41:27 +0300

    Fin sprint 10

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 from .validators import real_age
+
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    tag = models.CharField('Тег', max_length=20)
+
+    def __str__(self):
+        return self.tag
 
 
 class Birthday(models.Model):
@@ -12,6 +23,18 @@ class Birthday(models.Model):
     birthday = models.DateField('Дата рождения', validators=(
         real_age,))  # Точно так же применяется и к форме
     image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+    author = models.ForeignKey(
+        User, verbose_name='Автор записи', on_delete=models.CASCADE, null=True
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте Ctrl для выбора нескольких вариантов'
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
     class Meta:
         constraints = (
@@ -23,4 +46,17 @@ class Birthday(models.Model):
 
     def get_absolute_url(self):
         return reverse("birthday:detail", kwargs={"pk": self.pk})
-    
+
+
+class Congratulation(models.Model):
+    text = models.TextField('Текст поздравления')
+    birthday = models.ForeignKey(
+        Birthday,
+        on_delete=models.CASCADE,
+        related_name='congratulations'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('created_at',)
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,16 +1,33 @@
-# from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 # from django.core.paginator import Paginator
 from django.views.generic import (
     ListView, CreateView, UpdateView, DeleteView, DetailView)
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
 
-from .forms import BirthdayForm
+
+from .forms import BirthdayForm, CongratulationForm
 from .utils import calculate_birthday_countdown
-from .models import Birthday
+from .models import Birthday, Congratulation
 
 
 class BirthdayMixin:
     model = Birthday
+
+
+class OnlyAuthorMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user
+
+
+@login_required
+def simple_view(request):
+    return HttpResponse('Страница для залогиненных пользователей!')
 
 
 '''
@@ -49,7 +66,7 @@ def birthday(request, pk=None):
 '''
 
 
-class BirthdayCreateView(BirthdayMixin, CreateView):
+class BirthdayCreateView(BirthdayMixin, LoginRequiredMixin, CreateView):
     # model = Birthday
     # # Этот класс сам может создать форму на основе модели!
     # # Нет необходимости отдельно создавать форму через ModelForm.
@@ -63,8 +80,14 @@ class BirthdayCreateView(BirthdayMixin, CreateView):
     # success_url = reverse_lazy('birthday:list')
     form_class = BirthdayForm
 
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form)
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
+
+class BirthdayUpdateView(BirthdayMixin, OnlyAuthorMixin, UpdateView):
     # model = Birthday
     # form_class = BirthdayForm
     # template_name = 'birthday/birthday.html'
@@ -83,10 +106,14 @@ def birthday_list(request):
 '''
 
 
-class BirthdayListView(ListView, BirthdayMixin):
+class BirthdayListView(BirthdayMixin, ListView):
     # model = Birthday
+    # По умолчанию этот класс 
+    # выполняет запрос queryset = Birthday.objects.all(),
+    # но мы его переопределим:
+    queryset = Birthday.objects.prefetch_related('tags').select_related('author')
     ordering = 'id'
-    paginate_by = 3
+    paginate_by = 10
 
 
 '''
@@ -106,7 +133,7 @@ def delete_birthday(request, pk):
 '''
 
 
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
+class BirthdayDeleteView(BirthdayMixin, OnlyAuthorMixin, DeleteView):
     # model = Birthday
     success_url = reverse_lazy('birthday:list')
 
@@ -122,5 +149,31 @@ class BirthdayDetailView(DetailView):
             # Дату рождения берём из объекта в словаре context:
             self.object.birthday
         )
+        # Записываем в переменную form пустой объект формы.
+        context['form'] = CongratulationForm()
+        # Запрашиваем все поздравления для выбранного дня рождения.
+        context['congratulations'] = (
+            # Дополнительно подгружаем авторов комментариев,
+            # чтобы избежать множества запросов к БД.
+            self.object.congratulations.select_related('author')
+        )
         return context
 
+
+@login_required
+def add_comment(request, pk):
+    # Получаем объект дня рождения или выбрасываем 404 ошибку.
+    birthday = get_object_or_404(Birthday, pk=pk)
+    # Функция должна обрабатывать только POST-запросы.
+    form = CongratulationForm(request.POST)
+    if form.is_valid():
+        # Создаём объект поздравления, но не сохраняем его в БД.
+        congratulation = form.save(commit=False)
+        # В поле author передаём объект автора поздравления.
+        congratulation.author = request.user
+        # В поле birthday передаём объект дня рождения.
+        congratulation.birthday = birthday
+        # Сохраняем объект в БД.
+        congratulation.save()
+    # Перенаправляем пользователя назад, на страницу дня рождения.
+    return redirect('birthday:detail', pk=pk)
